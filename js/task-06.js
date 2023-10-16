cc04144be881dc592b55ae2ad64d03e4e6dc8554
lexandr10 <alexandrenko3006@gmail.com>
2023-10-17 00:30:10 +0300

    Js-change link

---
+++ js/task-06.js
@@ -0,0 +1,11 @@
+// const input = document.querySelector('#validation-input');
+// function validImput(evt) {
+//   if (evt.currentTarget.value.length === input.data - length) {
+//     input.classList.add('valid');
+//     input.classList.remove('invalid');
+//   } else {
+//     input.classList.remove('valid');
+//     input.classList.add('invalid');
+//   }
+// }
+// input.addEventListener('blur', validImput);
