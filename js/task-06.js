5967c713d0545439d2f2472edfa244748cd98017
lexandr10 <alexandrenko3006@gmail.com>
2023-10-17 14:59:10 +0300

    js ds 15

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,14 @@
-// const input = document.querySelector('#validation-input');
-// function validImput(evt) {
-//   if (evt.currentTarget.value.length === input.data - length) {
-//     input.classList.add('valid');
-//     input.classList.remove('invalid');
-//   } else {
-//     input.classList.remove('valid');
-//     input.classList.add('invalid');
-//   }
-// }
-// input.addEventListener('blur', validImput);
+const input = document.querySelector('#validation-input');
+function validImput(evt) {
+  if (
+    Number(evt.currentTarget.value.trim().length) ===
+    Number(evt.currentTarget.dataset.length)
+  ) {
+    input.classList.add('valid');
+    input.classList.remove('invalid');
+  } else {
+    input.classList.remove('valid');
+    input.classList.add('invalid');
+  }
+}
+input.addEventListener('blur', validImput);
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,10 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btn = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+const bg = document.body;
+function changeColor(ent) {
+  color.textContent = bg.style.backgroundColor = getRandomHexColor();
+}
+btn.addEventListener('click', changeColor);
